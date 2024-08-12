dc57834a93f426a4b8bd40943835a8232ab5b1f2
nryuji514 <e1922048@oit.ac.jp>
2024-08-12 15:28:44 +0900

    マイグレーションファイル作成

--- database/migrations/2024_08_11_063009_create_users_table.php
+++ database/migrations/2024_08_11_063009_create_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
